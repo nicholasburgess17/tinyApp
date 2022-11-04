2e634c1c6d5f78704bff0ab623c3fd3724af441e
Nicholas Burgess <Nicholas.Burgess13@gmail.comgit config --global user.name nicholas>
2022-11-04 20:19:56 +0000

    add registration capabilities and account for user errors

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+//helper functions
 const generateRandomString = (length) => {
   let string = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   result = " ";
@@ -13,6 +14,14 @@ const generateRandomString = (length) => {
   }
   return result;
 };
+const findUserByEmail = (email) => {
+  for (let userID in users) {
+    if (users[userID].email === email) {
+      return users[userID]
+    }
+  }
+};
+//objects
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -46,16 +55,19 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   const value = req.body.users;
   res.clearCookie("user_id", value);
-  res.redirect("/urls")
-})
+  res.redirect("/urls");
+});
 //main page
 app.get("/urls", (req, res) => {
-  const templatevars = { urls: urlDatabase, user: req.cookies["user_id"] };
+  const templatevars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
   res.render("urls_index", templatevars);
 });
 //add new urls
 app.get("/urls/new", (req, res) => {
-  const templatevars = { user: users };
+  const templatevars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templatevars);
 });
 //individual url pages
@@ -63,7 +75,7 @@ app.get("/urls/:id", (req, res) => {
   const templatevars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    user: req.cookies["user_id"],
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templatevars);
 });
@@ -73,45 +85,70 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
-//register
-//not displaying users.email in header, caused by templatevars... how to fix
+//register users
 app.get("/register", (req, res) => {
-  const templatevars = { urls: urlDatabase,  user:  req.cookies["user_id"]
-  }
-  res.render("register", templatevars)
-})
+  const templatevars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("register", templatevars);
+});
+//error 400, empty email or password when registering
+app.get("/error_page", (req, res) => {
+  const templatevars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("error_page", templatevars)
+});
+//user already exists
+app.get("/error", (req, res) => {
+  const templatevars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
+  res.render("error", templatevars)
+});
 //create short urls
 app.post("/urls", (req, res) => {
   const id = generateRandomString(6);
   urlDatabase[id] = req.body.longURL; // save new url to database
-  return res.redirect(`/urls/${id}`); 
+  return res.redirect(`/urls/${id}`);
 });
-//remove urls 
+//remove urls
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls/");
 });
 //add new urls to url database
 app.post("/urls/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   urlDatabase[id] = req.body.longURL;
- res.redirect("/urls");
+  res.redirect("/urls");
 });
 //create user ID
 app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
   const id = generateRandomString(4).trim();
-  const email = req.body.email
-  const password = req.body.password
   users[id] = {
     id,
     email,
     password,
+  };
+ //if email or pass are empty strings send response with 400 status code
+  if (!email|| !password) {
+    res.redirect("/error_page")
+    return;
   }
-//  const user = users[id]
-  res.cookie("user_id", id)
-  res.redirect("/urls")
-console.log(user.email)
-})
+  //if someone registers with an already registered email send response with a 400 erorr code
+  if (findUserByEmail(email)) {
+    res.redirect("/error")
+    return;
+  }
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
